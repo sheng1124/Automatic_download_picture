efaa7d6c081bfea4325ae61890e3a8f99bff3313
Sheng <49240374+sheng1124@users.noreply.github.com>
2020-04-15 18:32:12 +0800

    Add files via upload

---
+++ windows版/ADjpg.bat
@@ -0,0 +1,17 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set name="riku-minato"
+set n=265
+set m=12
+
+mkdir %name%
+cd %name%
+
+for /l %%i in (1,1,!n!) do (
+    for /l %%j in (1,1,!m!) do (
+        wget -O %name%-%%i-%%j.jpg https://jjgirls.com/japanese/%name%/%%i/%name%-%%j.jpg
+    )
+)
+
+pause
